c6ffb11026691acb5daeb57a1b729486f649e269
Olatoye Toyeeb <olatoyetoyeeb5@gmail.com>
2026-08-15 21:22:41 +0100

    Add basic student information printing

---
+++ report card printer.py
@@ -0,0 +1,12 @@
+name = 'Toyeeb'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 22
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+
